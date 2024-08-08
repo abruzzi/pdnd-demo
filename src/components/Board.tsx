578709fd981e45ebcb042c6dd9403eb64f758e9b
Juntao Qiu <juntao.qiu@gmail.com>
2024-08-08 20:47:10 +1000

    restyling card a bit

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -7,7 +7,7 @@ const Board = () => {
   return (
     <div className="flex flex-col m-auto bg-gray-100 h-screen overflow-auto">
       <h1 className="mx-10 my-4 text-2xl">{board.name}</h1>
-      <ol className="max-h-full mx-10 my-4 flex flex-row gap-6 flex-grow">
+      <ol className="max-h-full mx-10 mb-8 flex flex-row gap-6 flex-grow">
         {board.columns.map((col) => (
           <Column key={col.id} column={col} />
         ))}
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,14 +1,17 @@
 import { CardType } from "../type.tsx";
 
 export const Card = ({ card }: { card: CardType }) => {
-  const { id, title, position, columnId } = card;
+  const { id, title } = card;
 
   return (
     <li
       data-test-id={id}
-      className="relative p-2 bg-gray-100 rounded-md text-lg hover:cursor-grab"
+      className="relative p-2 bg-gradient-to-br from-slate-100 to-slate-200 drop-shadow-sm rounded-md text-lg"
     >
-      <p>{title}</p>
+      <span className="bg-orange-500 text-white rounded-sm py-0.5 px-1 text-xs text-center">
+        {id}
+      </span>
+      <p className="text-slate-800">{title}</p>
     </li>
   );
 };
